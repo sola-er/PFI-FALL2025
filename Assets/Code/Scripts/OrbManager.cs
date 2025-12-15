4d12cbdf802c0618f6b401d2207125171cd705ed
manel <raniahouili@gmail.com>
2025-12-15 14:47:38 -0500

    we're washed of monday humiliation
    - added tag to houses
    - added orbmanager script and it works !!!!!!

--- Assets/Code/Scripts/OrbManager.cs
+++ Assets/Code/Scripts/OrbManager.cs
@@ -1,16 +1,44 @@
+using System;
 using UnityEngine;
+using System.Collections.Generic;
+using static System.Random;
 
 public class OrbManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject orbPrefab;
+
+    private List<GameObject> spawnedOrbs = new List<GameObject>();
+    private List<GameObject> houses;
+
+    private void Awake()
     {
-        
+        GameObject[] houseObjects = GameObject.FindGameObjectsWithTag("House");
+        houses = new List<GameObject>(houseObjects);
+        spawnedOrbs = new List<GameObject>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        for (int i = 0; i < houses.Count; ++i)
+            if (UnityEngine.Random.Range(0, 100) < 50)
+            {
+                GameObject orb = Instantiate(orbPrefab, houses[i].transform);
+                spawnedOrbs.Add(orb);
+            }
     }
+
+    private void Collect(GameObject orb)
+    {
+        if (spawnedOrbs.Contains(orb))
+        {
+            Destroy(orb);
+            spawnedOrbs.Remove(orb);
+        }
+
+        if (RemainingOrbs() == 0)
+            Debug.Log("gg senpai >.<");
+    }
+    
+    public int RemainingOrbs()
+        => spawnedOrbs.Count;
 }
